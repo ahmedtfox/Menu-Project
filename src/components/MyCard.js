01fcc49effb538881510e8d73bbbf182a23335db
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-22 18:09:28 +0300

    add animation

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,23 +1,28 @@
 import { Card, Row, Col } from "react-bootstrap";
+import { Fade, Zoom } from "react-awesome-reveal";
+
 function MyCard(props) {
   const item = props.item;
   return (
-    <Row>
-      <Col sm="12">
-        <Card className="flex-row d-flex">
-          <div className="img-item">
-            <Card.Img variant="top" src={item.ImageUrl} />
-          </div>
-          <Card.Body>
-            <div className="d-flex justify-content-between">
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Title>{item.price}جنيه</Card.Title>
+    <Zoom>
+      <Row className="my
+      -3">
+        <Col sm="12">
+          <Card className="flex-row d-flex">
+            <div className="img-item">
+              <Card.Img variant="top" src={item.ImageUrl} />
             </div>
-            <Card.Text>{item.description}</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+            <Card.Body>
+              <div className="d-flex justify-content-between">
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Title>{item.price}جنيه</Card.Title>
+              </div>
+              <Card.Text>{item.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Zoom>
   );
 }
 
